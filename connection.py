f4feea3ec11419265b94a6afb8cafcbff122d903
Eva Nedeliaková <evanedeliakova@gmail.com>
2021-05-05 16:15:49 +0200

    Update connection.py

--- connection.py
+++ connection.py
@@ -4,7 +4,11 @@ import pymongo
 host = os.getenv('MONGODB_HOST', 'localhost')
 port = os.getenv('MONGODB_PORT', '27017')
 
-connection = pymongo.MongoClient("mongodb://" + host + ":" + port + "/")
+user = os.getenv('MONGODB_USER', 'admin')
+password = os.getenv('MONGODB_PASS', 'password')
+
+
+connection = pymongo.MongoClient("mongodb://"+ user +":"+ password +"@" + host + ":" + port + "/")
 
 
 def get_db(db):
