6b7a34fb71ad4579002b1a516790a0a34b6bda90
AkiEvka <evanedeliakova@gmail.com>
2023-04-28 15:39:59 +0200

    fix keys

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM python
 WORKDIR /app
 COPY . .
 RUN pip install -r requirements.txt
+RUN mkdir keys
+RUN ssh-keygen -t rsa -m PEM -f keys/id_rsa
 CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
--- crypt.py
+++ crypt.py
@@ -2,8 +2,8 @@ import base64
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
-publicKey = RSA.importKey(open("keys/public.pem").read())
-privateKey = RSA.importKey(open("keys/private.pem").read())
+publicKey = RSA.importKey(open("keys/id_rsa.pem").read())
+privateKey = RSA.importKey(open("keys/id_rsa.pem").read())
 
 cipherEncrypt = PKCS1_OAEP.new(publicKey)
 cipherDecrypt = PKCS1_OAEP.new(privateKey)
